08784e9d60d4a66c17c80a10b0e317f5912a84a4
SilasCundiff <silas@Rainbow.localdomain>
2022-06-11 20:50:10 -0400

    Add basic auth flow and sessions

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,18 @@
+import { signOut } from "next-auth/react";
 
+const IndexPage = () => {
+  return (
+    <div className="flex flex-col items-center bg-gradient-to-b  from-black to-zinc-900 min-h-screen w-full justify-center">
+      <div>
+        <button
+          className="bg-[#18D860] text-black px-4 py-3 font-semibold  rounded-full w-48 mb-2"
+          onClick={() => signOut()}
+        >
+          Logout
+        </button>
+      </div>
+    </div>
+  );
+};
 
-const IndexPage = () => (
- <div>
-    <h1>Hello Next.js</h1>
- </div>
-)
-
-export default IndexPage
+export default IndexPage;
