f16bd3f2df76a7d5fa6fdae2fc283f4139b6e9b3
SilasCundiff <silinski23@gmail.com>
2023-11-30 11:18:31 -0500

    remove default playlist

--- hooks/usePlaylist.ts
+++ hooks/usePlaylist.ts
@@ -2,7 +2,7 @@ import { create } from "zustand";
 import { PlaylistStore, SelectedPlaylist } from "../types";
 
 export const useSelectPlaylist = create<SelectedPlaylist>((set) => ({
-  playlist: { id: "1MGSI5VKz9MGcCKboUuTmX", name: "The Dark Pool" },
+  playlist: { id: null },
   setPlaylist: (playlistId: string) => set({ playlist: { id: playlistId } }),
 }));
 
